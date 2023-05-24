d0e1c64d4a4e536c0cf8dec65d50dc495e58788e
Colin Foulkes <cfoulkes@protegra.com>
2023-05-24 06:22:17 -0500

    refactor edit form to use input components

--- Customizable-FE/src/app/edit-form-control/edit-form-control.component.ts
+++ Customizable-FE/src/app/edit-form-control/edit-form-control.component.ts
@@ -11,11 +11,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { JsonFormControl } from '../json-form/json-form.component';
 import { SectionHeaderComponent } from '../json-form/section-header/section-header.component';
+import { InputTextComponent } from '../json-form/input-text/input-text.component';
+import { InputSelectComponent } from '../json-form/input-select/input-select.component';
+import { InputCheckboxComponent } from '../json-form/input-checkbox/input-checkbox.component';
+import { InputDatepickerComponent } from '../json-form/input-datepicker/input-datepicker.component';
 
 @Component({
   selector: 'app-edit-form-control',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, CdkMenuModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCheckboxModule, MatDatepickerModule, SectionHeaderComponent],
+  imports: [CommonModule, ReactiveFormsModule, CdkMenuModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCheckboxModule, MatDatepickerModule, InputTextComponent, InputSelectComponent, InputCheckboxComponent, InputDatepickerComponent, SectionHeaderComponent],
   templateUrl: './edit-form-control.component.html',
   styleUrls: ['./edit-form-control.component.scss']
 })
